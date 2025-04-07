0a80d043d65b7016bc1d596c853bf5eab433ebb6
An-ixa <annapham007@gmail.com>
2025-04-07 12:43:44 +0200

    Vytvoření karty pacienta - přidání validací vložených dat

--- server/src/controllers/patientCardController.js
+++ server/src/controllers/patientCardController.js
@@ -1,9 +1,16 @@
 import * as patientService from '../services/patientCardService.js';
-import { isValidBirthNumber } from '../utils/validator.js';
+import { isValidBirthNumber, isOnlyLetters, isPositiveNumber, isValidPhoneNumber,isValidPostCode } from '../utils/validator.js';
 
 export async function addPatient(req, res) {
     const body = req.body;
     const required = ['first_name', 'last_name', 'date_of_birth', 'birth_number', 'insurance_id', 'sex'];
+    const notRequired = ['contact_info.contact_phone', 'weight', 'height', 'address.address_zip_code'];
+
+    for (let key in body) {
+        if (typeof body[key] === 'string') {
+          body[key] = body[key].trim();
+        }
+    }
 
     for (const field of required) {
         if (!body[field]) {
@@ -12,7 +19,32 @@ export async function addPatient(req, res) {
     }
 
     if (!isValidBirthNumber(body.birth_number)) {
-        return res.status(400).json({ error: 'Invalid birth number format.' });
+        return res.status(400).json({ error: 'Invalid birth number format. Correct format: XXXXXX/XXXX' });
+    }
+
+    if (!isOnlyLetters(body.first_name)) {
+        return res.status(400).json({ error: 'Invalid first name format. Only letters are allowed.' });
+    }
+
+    if (!isOnlyLetters(body.last_name)) {
+        return res.status(400).json({ error: 'Invalid last name format. Only letters are allowed.' });
+    }
+
+    // Not required
+    if (!isValidPhoneNumber(body.contact_info.contact_phone) && body.contact_info.contact_phone != null) {
+        return res.status(400).json({ error: 'Invalid phone number format. Correct format: +XXXXXXXXXXXX or XXXXXXXXX.' });
+    }
+
+    if (!isPositiveNumber(body.weight) && body.weight != null) {
+        return res.status(400).json({ error: 'Invalid weight format. Weight must be a positive number.' });
+   }
+
+    if (!isPositiveNumber(body.height) && body.height != null) {
+        return res.status(400).json({ error: 'Invalid height format. Height must be a positive number.' });
+    }
+
+    if (!isValidPostCode(body.address.address_zip_code) && body.address.address_zip_code != null) {
+        return res.status(400).json({ error: 'Invalid post code format. Correct format: XXX XX or XXXXX.' });
     }
 
     try {
--- server/src/utils/validator.js
+++ server/src/utils/validator.js
@@ -1,4 +1,27 @@
 export function isValidBirthNumber(birthNumber) {
-    const regex = /^\d{6}\/\d{3,4}$/;
+    const regex = /^\d{6}\/\d{4}$/;
     return regex.test(birthNumber);
 }
+
+export function isOnlyLetters(name) {
+    const trimmedName = String(name).trim()
+    const regex = /^[A-Za-zÁ-žá-ž]+$/;
+    return regex.test(trimmedName);
+}
+
+export function isValidPhoneNumber(phoneNumber) {
+    const regex = /^\+?\d{9,13}$/;
+    return regex.test(phoneNumber);
+}
+
+export function isPositiveNumber(num) {
+    const regex = /^(?:\d+|\d*\.\d+)$/;
+    return regex.test(num) && parseFloat(num) > 0;
+}
+
+export function isValidPostCode(postCode) {
+    const postCodeBS = String(postCode).replace(" ","");
+    const regex = /^\d{5}$/;
+    return regex.test(postCodeBS);
+}
+
