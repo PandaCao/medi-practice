8df8a0c229ac6b023f73b63f3f8e9eec178b7d32
Ondřej Šabrňák <ondrej@sabrnak.com>
2025-04-09 13:42:56 +0200

    refactor(app): simplify CORS configuration to allow all origins for API requests

--- server/src/app.js
+++ server/src/app.js
@@ -5,27 +5,10 @@ import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 5000;
 
-// CORS configuration
-const allowedOrigins = [
-    'http://localhost:3000', // Allow requests from local development
-    'https://medipractise-server.onrender.com',
-];
-
-// Enable CORS with dynamic origin
+// Enable CORS for all origins
 app.use(
     cors({
-        origin: function (origin, callback) {
-            // allow requests with no origin (like mobile apps or curl requests)
-            if (!origin) return callback(null, true);
-
-            if (allowedOrigins.indexOf(origin) === -1) {
-                const msg =
-                    'The CORS policy for this site does not allow access from the specified Origin.';
-                return callback(new Error(msg), false);
-            }
-            return callback(null, true);
-        },
-        credentials: true,
+        origin: '*', // Allow all origins
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
     }),
