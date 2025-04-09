51f753ac4b129e6de94030ce723084889a370ff2
Ondřej Šabrňák <ondrej@sabrnak.com>
2025-04-09 13:45:27 +0200

    refactor(app): enhance CORS configuration to restrict origins and improve security

--- server/src/app.js
+++ server/src/app.js
@@ -5,12 +5,30 @@ import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Enable CORS for all origins
+// CORS configuration
+const allowedOrigins = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://medipractise-server.onrender.com',
+    'https://medipractise.onrender.com',
+];
+
 app.use(
     cors({
-        origin: '*', // Allow all origins
+        origin: function (origin, callback) {
+            // allow requests with no origin (like mobile apps or curl requests)
+            if (!origin) return callback(null, true);
+
+            if (allowedOrigins.indexOf(origin) === -1) {
+                const msg =
+                    'The CORS policy for this site does not allow access from the specified Origin.';
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        },
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+        credentials: true,
     }),
 );
 
