f0cefcadf5061eb2107dd55e8dc26f27f33ba187
Ondřej Šabrňák <ondrej@sabrnak.com>
2025-04-11 11:10:03 +0200

    refactor(app): streamline CORS configuration by reducing allowed origins and removing unnecessary options

--- server/src/app.js
+++ server/src/app.js
@@ -6,34 +6,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // CORS configuration
-const allowedOrigins = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'https://medipractise-server.onrender.com',
-    'https://medipractise.onrender.com',
-];
-
-// Enable CORS pre-flight
-app.options('*', cors());
-
 app.use(
     cors({
-        origin: function (origin, callback) {
-            // allow requests with no origin (like mobile apps or curl requests)
-            if (!origin) return callback(null, true);
-
-            if (allowedOrigins.indexOf(origin) === -1) {
-                const msg =
-                    'The CORS policy for this site does not allow access from the specified Origin.';
-                return callback(new Error(msg), false);
-            }
-            return callback(null, true);
-        },
+        origin: ['http://localhost:3000', 'https://medipractise.onrender.com'],
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-        credentials: true,
-        preflightContinue: false,
-        optionsSuccessStatus: 204,
     }),
 );
 
