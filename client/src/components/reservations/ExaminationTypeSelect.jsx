f6ea14cfc7ab0c2c3fe4127516eb02911223ad3f
Ondřej Šabrňák <ondrej@sabrnak.com>
2025-05-07 12:00:16 +0200

    refactor(ExaminationTypeSelect): standardize option values and clean up formatting for examination type selection

--- client/src/components/reservations/ExaminationTypeSelect.jsx
+++ client/src/components/reservations/ExaminationTypeSelect.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
 
-function ExaminationTypeSelect({ value, onChange , otherValue, onOtherChange}) {
-
+function ExaminationTypeSelect({ value, onChange, otherValue, onOtherChange }) {
     return (
         <Form.Group className="mb-3">
             <Form.Label>Typ vyšetření</Form.Label>
@@ -13,15 +12,16 @@ function ExaminationTypeSelect({ value, onChange , otherValue, onOtherChange}) {
                 required
             >
                 <option value="">Vyberte typ vyšetření</option>
-                <option value="preventivni prohlidka">Preventivní prohlídka</option>
-                <option value="vysetreni">Vyšetření</option>
-                <option value="krevní test">Krevní test</option>
-                <option value="ultrazvuk">Ultrazvuk</option>
-                <option value="rentgen">Rentgen</option>
+                <option value="Preventivní prohlídka">
+                    Preventivní prohlídka
+                </option>
+                <option value="Vyšetření">Vyšetření</option>
+                <option value="Krevní test">Krevní test</option>
+                <option value="Ultrazvuk">Ultrazvuk</option>
+                <option value="Rentgen">Rentgen</option>
                 <option value="EKG">EKG</option>
             </Form.Select>
         </Form.Group>
-
     );
 }
 
